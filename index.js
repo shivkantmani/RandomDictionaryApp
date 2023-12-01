102687313479336e2f00c57b4436b1f59bfa88b3
Shiv Kant Mani <Shivkant4852@gmail.com>
2023-12-01 23:40:16 +0530

    modify files of Random Dictionary app

---
+++ index.js
@@ -0,0 +1,30 @@
+import  express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", async (req,res) =>{
+    try{
+        const response = await axios.get(`https://api.api-ninjas.com/v1/randomword`,{
+            headers: { 
+                'x-api-key': 'Nl8tPW0npSmuZpwArRIfxA==Ax4f2Gsvm0dez8T7'
+              }
+        })
+        const result = response.data;
+        const response2 = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${result}`);
+        const result2 = response2.body;
+        console.log(result2);
+    }
+    catch (error){
+        console.log("error");
+    }
+})
+
+app.listen(port, () =>{
+    console.log(`Running on port ${port}.`)
+})
